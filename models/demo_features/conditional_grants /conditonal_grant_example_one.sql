849f6abe6ff5ab5db690d02cd0d6585e41968d6f
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-01-05 00:00:22 +0000

    updates

---
+++ models/demo_features/conditional_grants /conditonal_grant_example_one.sql
@@ -0,0 +1,15 @@
+-- example of using the conditonal grant macro to grant different permissions depending on the target
+
+{{ config(
+    materialized = 'table', 
+    post_hook = conditonal_grant(
+                    { 'default': { 'select': ['transformer', 'steve_d_demo_role'], 'insert': ['transformer', 'steve_d_demo_role'] }, 
+                      'prod': { 'select': ['transformer', 'steve_d_demo_role']} 
+                    })
+) }}
+
+
+Select 
+    'this is great!' as my_frist_column,
+    1/1 as a_simple_calculation,
+    current_timestamp() as current_time
--- models/general_models/samples/my_first_model.sql
+++ models/general_models/samples/my_first_model.sql
@@ -1,13 +1,3 @@
-{{ config(
-    materialized = 'table', 
-    post_hook = conditonal_grant(
-                    { 'default': { 'select': ['transformer', 'steve_d_demo_role'], 'insert': ['transformer', 'steve_d_demo_role'] }, 
-                      'prod': { 'select': ['transformer', 'steve_d_demo_role']} 
-                    })
-) }}
-
-
-
 
 Select 
     'this is great!' as my_frist_column,
