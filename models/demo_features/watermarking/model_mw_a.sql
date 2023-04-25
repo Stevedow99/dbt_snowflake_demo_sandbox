a8a67d11e6a78ddbeb15f19feede87e81fbf160b
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-04-25 01:34:59 +0000

    updates

---
+++ models/demo_features/watermarking/model_mw_a.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+  ID,
+  STATUS,
+  CREATED_TIMESTAMP,
+  MODIFIED_TIMESTAMP
+FROM {{ source('sample_data', 'example_orders_table') }}
+WHERE MODIFIED_TIMESTAMP > {{get_high_watermark('model_mw_b', 'MODIFIED_TIMESTAMP')}}
