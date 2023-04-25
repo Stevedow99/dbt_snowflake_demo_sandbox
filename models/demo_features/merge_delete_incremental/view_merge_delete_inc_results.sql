bae97345c78d728be6f553de3af7afe296d50a51
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-04-25 18:03:08 +0000

    updates

--- models/demo_features/merge_delete_incremental/view_merge_delete_inc_results.sql
+++ models/demo_features/merge_delete_incremental/view_merge_delete_inc_results.sql
@@ -5,3 +5,12 @@ Select
 *
 from
 {{ ref('merge_delete_incremental_table') }}
+
+
+-- compare ids
+Select
+a.id as target_id,
+b.id as source_id
+from {{ ref('merge_delete_incremental_table') }} a
+FULL OUTER JOIN {{ source('sample_data', 'example_orders_table_two') }} b
+    on a.id = b.id
