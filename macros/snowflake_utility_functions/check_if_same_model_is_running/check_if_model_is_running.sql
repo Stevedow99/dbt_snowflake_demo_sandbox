fe046bd69c8c8fda0f4d88d78094001c72835cfc
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-12-18 01:59:08 +0000

    updates

--- macros/snowflake_utility_functions/check_if_same_model_is_running/check_if_model_is_running.sql
+++ macros/snowflake_utility_functions/check_if_same_model_is_running/check_if_model_is_running.sql
@@ -10,18 +10,20 @@
 
     {%- endset -%}
 
-    {%- set results = run_query(get_running_queries_with_tag) -%}
 
     {%- if execute -%}
 
-        {%- set number_of_queries_running = results.columns[0].values()[0] -%}
-
         {%- for _ in range(0, number_of_times_to_check) -%}
+
+            {%- set results = run_query(get_running_queries_with_tag) -%}
+
+            {%- set number_of_queries_running = results.columns[0].values()[0] -%}
             
             {%- if number_of_queries_running > 0 -%}
 
                 {{ log("There is currently processes running with the query tag " ~ query_tag_to_check) }}
                 {{ log("waiting " ~ polling_period_in_seconds ~ " seconds and then checking again "  ) }}
+                {{ log("There are " ~ running_example_model ~ " processes running")}}
                 
                 {%- do run_query( 'CALL SYSTEM$WAIT(' ~ polling_period_in_seconds ~ ')') -%}
                 
