698415bd70f3aeca2c1d942ddbb32a1e0315acd8
Steve Dowling <steve.dowling@fishtownanalytics.com>
2024-04-28 20:49:05 +0000

    updates

--- models/demo_features/external_tables/cities_per_state_reporting_table.sql
+++ models/demo_features/external_tables/cities_per_state_reporting_table.sql
@@ -8,6 +8,6 @@
 select
     State,
     count(*) as number_of_cities
-from {{ ref('stg_snowflake_external_s3_stage__city_data') }}
+from {{ ref("stg_snowflake_external_s3_stage__city_data") }}
 group by State
 order by number_of_cities desc
