058eb79027f9886a32c3fd89f368ddc2a3c59aef
Steve Dowling <steve.dowling@fishtownanalytics.com>
2024-04-28 18:40:34 +0000

    updates

--- macros/refs.sql
+++ macros/refs.sql
@@ -1,5 +1,11 @@
-{%- macro log_input() -%}
+{% macro default__log_input(input_message) %}
 
-    {{ 'this is the project macro' }}
+    {{ 'project' }}
 
-{%- endmacro -%}
+{% endmacro %}
+
+
+
+{%- macro log_input(input_message) -%}
+    {{ return(adapter.dispatch('log_input', 'dbt_snowflake_demo_sandbox')(input_message)) }}
+{% endmacro %}
