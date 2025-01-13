648e6c48578199533654aeb1633dc5ff3472f58f
Steve Dowling <steve.dowling@fishtownanalytics.com>
2025-01-13 21:22:02 +0000

    updates

--- models/demo_features/iceberg_incrementals/iceberg_incremental_table_new.sql
+++ models/demo_features/iceberg_incrementals/iceberg_incremental_table_new.sql
@@ -10,11 +10,7 @@
 }}
 
 
-select 
-    id,
-    status,
-    CAST(CREATED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS CREATED_TIMESTAMP,
-    CAST(MODIFIED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS MODIFIED_TIMESTAMP
+select * 
 from {{ source('sample_data', 'example_orders_table_two') }}
 
 
