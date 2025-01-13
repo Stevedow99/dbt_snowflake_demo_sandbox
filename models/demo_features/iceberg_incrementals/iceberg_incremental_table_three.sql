5b76102091ecc9d635fc0a4e4a17b86d78924dcd
Steve Dowling <steve.dowling@fishtownanalytics.com>
2025-01-13 22:36:46 +0000

    updates

--- models/demo_features/iceberg_incrementals/iceberg_incremental_table_three.sql
+++ models/demo_features/iceberg_incrementals/iceberg_incremental_table_three.sql
@@ -10,11 +10,15 @@
 }}
 
 
-select * 
+select 
+    id,
+    status,
+    CAST(CREATED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS CREATED_TIMESTAMP,
+    CAST(MODIFIED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS MODIFIED_TIMESTAMP
 from {{ source('sample_data', 'example_orders_table_two') }}
 
 
-{% if is_incremental_custom_check() %}
+{% if is_incremental() %}
 
   where modified_timestamp > (select max(modified_timestamp) from {{ this }})
 
